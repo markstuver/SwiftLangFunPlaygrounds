d767df2217e5f00eaaf381eaa220f84c4cbe680d
Mark Stuver <mark@stuverfamily.com>
2015-05-04 22:22:02 -0400

    Initial Upload of Udemy Playgrouns

---
+++ ControlFlow/BasicIfStatements.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Control Flow handles the flow of the program through the statements
+
+
+// BASIC: IF STATEMENTS
+
+// EXAMPLE 1
+var apples = 10
+if apples > 0
+{
+    println("There are some apples left")
+} else {
+    println("There are no apples left!")
+}
+
+// EXAMPLE 2
+var testScore = 99
+if testScore == 100
+{
+    println("Perfect Score")
+} else {
+    println("Less than perfect score")
+}
+
+// EXAMPLE 3
+var gpa = 2.9
+if gpa < 3.0
+{
+    println("You have less than a B average")
+}else {
+    println("You have a B average or higher")
+}
+
+
