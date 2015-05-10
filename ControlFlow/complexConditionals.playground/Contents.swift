6cc3fdc8b9ec3f5dc2f0debfb65c1e10f2999484
Mark Stuver <mark@stuverfamily.com>
2015-05-10 14:56:27 -0400

    Update complexConditionals

--- ControlFlow/complexConditionals.playground/Contents.swift
+++ ControlFlow/complexConditionals.playground/Contents.swift
@@ -2,3 +2,17 @@
 
 import UIKit
 
+var citizen = true
+var age = 19
+
+// Conditional: if the citizen variable IS true AND the age is greater than 18
+// If the statement is true, due code in the brackets. If not true, skip the code in brackets.
+
+if citizen == true && age > 18
+{
+    println("You are eligible to vote")
+}
+
+
+// See BitFountain Playground for more ControlFlow info. 
+
