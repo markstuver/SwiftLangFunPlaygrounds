9834f187789f5e4123076c56157cf4bfd9356853
Mark Stuver <mark@stuverfamily.com>
2015-07-06 23:05:09 -0400

    Lab 7 Completed

--- Lab Challenges/SLA_Lab7.playground/Contents.swift
+++ Lab Challenges/SLA_Lab7.playground/Contents.swift
@@ -8,7 +8,7 @@ July 5th 2015
 SLA_Lab 7
 */
 
-// TASK #3 - Type in the provided code:
+ // TASK #3 - Type in the provided code:
 
 func feetToInchesInt(feet:Int) -> Double {
     
@@ -45,34 +45,90 @@ println(feetToInchesFloat(164.55))
 println(inchesToFeetInt(1000))
 
 
+
 //TASK #4 - Create 2 Functions that encapsulate the methods above using Generics
 
-extension Double {
-    
-    var multiplyby12 : Double<T> {return self * 12}
-    var divideby12 : Double {return self / 12}
-    
-}
 
+// FUNCTION TO CONVERT FEET INTO INCHES - with generic parameter that returns a Double
 func feetToInches<T: Equatable>(feet:T) -> Double {
 
-    let valueOfFeet = Double(feet)
+    // Variable set as a Double
+    var feetValue = 0.0
     
-    return returnValue
+// if the data passed into function is a Double
+    if feet is Double {
+       
+        // cast feet as a Double and multiply by 12
+        feetValue = (feet as! Double) * 12
+    }
+        
+// if the data passed into function is an Float
+    else if feet is Float {
+        
+        // Create instance equal to feet casted as a Float
+        let feetIntCasting = feet as! Float
+        // convert int into a Double and multipy by 12
+        feetValue = Double(feetIntCasting) * 12
+        
+        // If data passed is anything else
+    }
+    
+// if the data passed into function is an Int
+    else if feet is Int {
+        
+        // Create instance equal to feet casted as an Int
+        let feetIntCasting = feet as! Int
+        // convert int into a Double and multipy by 12
+        feetValue = Double(feetIntCasting) * 12
+    
+// If data passed is not Double/Float/Int
+    } else {
+        
+        // print error message
+        println("An error has occurred. Data type passed is not supported in this function")
+    }
+    
+    // return double value
+    return feetValue
 }
 
+
+// FUNCTION TO CONVERT INCHES INTO FEET - with Generic parameter and returns a Double
 
 func inchesToFeet<T: Equatable>(inches:T) -> Double {
     
-    return Double(inches)/12
+    var inchesValue = 0.0
+    
+// Data passed is a Double
+    if inches is Double {
+        inchesValue = (inches as! Double) / 12
+    }
+        
+// Data passed is an Int
+    else if inches is Int {
+        let valueCasting = inches as! Int
+        inchesValue = Double(valueCasting) / 12
+    }
+        
+// Data passed is a Float
+    else if inches is Float {
+        let valueCasting = inches as! Float
+        inchesValue = Double(valueCasting) / 12
+    }
+        
+// Data passed is not supported
+    else {
+        println("An error has occured. The data passed into this function is not supported at this time.")
+    }
+    
+    // return a Double value
+    return inchesValue
 }
 
+println(feetToInches(166.5))
 println(inchesToFeet(12))
 
 
-
-
-
-
-
-
+println(feetToInches(177))
+println(feetToInches(164.55))
+println(inchesToFeet(1000))
